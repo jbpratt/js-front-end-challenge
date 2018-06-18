5a8d3377c0c04248c4701fac7721c1a2fd1c7ecd
jbpratt <jbpratt78@gmail.com>
2018-06-18 10:55:05 -0500

    break; working form

--- angular6/src/app/app.component.ts
+++ angular6/src/app/app.component.ts
@@ -10,10 +10,13 @@ export class AppComponent {
   constructor(private movieService: MoviesService) {}
   title = "Angular Movie Search";
   keyword = "";
+  results = [];
+  loading = false;
 
   onSubmit() {
-    this.movieService.search(this.keyword).subscribe(result => {
-      console.log(result);
+    this.loading = true;
+    this.movieService.search(this.keyword).subscribe((result: any) => {
+      this.results = result.results;
     });
   }
 }
